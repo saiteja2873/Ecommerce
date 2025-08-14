df79c4a2dff5fd9ccbd5cb38dc1541018cdf8b06
saiteja2873 <pasamsaiteja2020@gmail.com>
2025-08-14 17:02:37 +0530

    removed the empty files

--- frontend/src/components/checkout/index.ts
+++
@@ -1,5 +0,0 @@
-// // src/components/checkout/index.ts
-// export { default as CheckoutForm } from "./checkoutForm";
-// export { default as AddressSection } from "./AddressSection";
-// export { default as OrderSummary } from "./OrderSummary";
-// export { default as PlaceOrderButton } from "./PlaceOrderButton";
