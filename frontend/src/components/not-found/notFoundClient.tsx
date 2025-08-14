924173f21f72ffba0bd0a520a82d7fa7ba4a9d9d
saiteja2873 <pasamsaiteja2020@gmail.com>
2025-08-14 22:16:50 +0530

    updated not-found page

--- frontend/src/components/not-found/notFoundClient.tsx
+++ frontend/src/components/not-found/notFoundClient.tsx
@@ -1,10 +1,16 @@
-// components/NotFoundClient.tsx
+// app/components/not-found/notFoundClient.tsx
 "use client";
 
 import { useSearchParams } from "next/navigation";
 
 export default function NotFoundClient() {
   const searchParams = useSearchParams();
-  const error = searchParams.get("error");
-  return <div>Page not found. Error: {error}</div>;
+  const errorCode = searchParams.get("code");
+
+  return (
+    <div>
+      <h1>Page Not Found</h1>
+      {errorCode && <p>Error Code: {errorCode}</p>}
+    </div>
+  );
 }
